0b65e72ad1d325962f535eeb065790675b08e2ad
devdgehog <72769867+devdgehog@users.noreply.github.com>
2020-10-17 01:18:33 +0200

    challenge setup

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,26 @@
 <template>
-  <div>
-    Working
-  </div>
+<div>
+  Build The Community Your Fans Will Love
+
+  Huddle re-imagines the way we build communities. You have a voice, but so does your audience.
+  Create connections with your users as you engage in genuine discussion.
+
+  Register
+
+  <footer>
+    <p class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="https://twitter.com/devdgehog">devdgehog</a>.
+    </p>
+  </footer>
+</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class App extends Vue {}
+export default class Main extends Vue {}
 </script>
 
 <style lang="scss">
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: 'Frontend Mentor | Huddle landing page with single introductory section',
+    },
+  },
+};
